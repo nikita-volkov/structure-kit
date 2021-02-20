d3df554ce174b8b54dff32a5fcdf3d325c017c6d
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-02-20 19:23:35 +0300

    Sketch out LruHashCache

---
+++ library/StructureKit/LruHashCache.hs
@@ -0,0 +1,20 @@
+module StructureKit.LruHashCache
+(
+  LruHashCache,
+  lookup,
+)
+where
+
+import StructureKit.Prelude hiding (lookup)
+import qualified StructureKit.LookupOrderedHashMap as LookupOrderedHashMap
+
+
+data LruHashCache k v =
+  LruHashCache
+    {-| Max size. -}
+    Int
+    (LookupOrderedHashMap.LookupOrderedHashMap k v)
+
+lookup :: (Hashable k, Eq k) => k -> LruHashCache k v -> (Maybe v, LruHashCache k v)
+lookup key (LruHashCache size map) =
+  error "TODO"
