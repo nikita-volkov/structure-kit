7def9d315a7eb5504e29a97dc0193a536b3c18ed
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-02-21 15:24:29 +0300

    Update LruHashCache

--- library/StructureKit/LruHashCache.hs
+++ library/StructureKit/LruHashCache.hs
@@ -2,19 +2,36 @@ module StructureKit.LruHashCache
 (
   LruHashCache,
   lookup,
+  insert,
 )
 where
 
-import StructureKit.Prelude hiding (lookup)
+import StructureKit.Prelude hiding (lookup, insert)
 import qualified StructureKit.TouchOrderedHashMap as TouchOrderedHashMap
 
 
 data LruHashCache k v =
   LruHashCache
     Int
-    {-^ Max size. -}
+    {-^ Slots occupied. -}
+    Int
+    {-^ Slots available. -}
     (TouchOrderedHashMap.TouchOrderedHashMap k v)
 
 lookup :: (Hashable k, Eq k) => k -> LruHashCache k v -> (Maybe v, LruHashCache k v)
-lookup key (LruHashCache size map) =
-  TouchOrderedHashMap.lookup key map & second (LruHashCache size)
+lookup key (LruHashCache occupied avail map) =
+  TouchOrderedHashMap.lookup key map & second (LruHashCache occupied avail)
+
+insert :: (Hashable k, Eq k) => k -> v -> LruHashCache k v -> (Maybe (k, v), LruHashCache k v)
+insert key value (LruHashCache occupied avail map) =
+  TouchOrderedHashMap.insert key value map & \(new, map) ->
+    case new of
+      True ->
+        case avail > 0 of
+          True ->
+            (Nothing, LruHashCache (succ occupied) (pred avail) map)
+          False ->
+            TouchOrderedHashMap.evict map
+              & second (LruHashCache occupied avail)
+      False ->
+        (Nothing, LruHashCache occupied avail map)
