91b7c2f9ad7792bdcb1b8eabdb48c9cc91e32fcc
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-02-21 13:27:34 +0300

    Avoid registering missing entries

--- library/StructureKit/LookupOrderedHashMap.hs
+++ library/StructureKit/LookupOrderedHashMap.hs
@@ -17,26 +17,27 @@ data LookupOrderedHashMap k v =
     (Hamt.Hamt (Entry k v))
 
 data Entry k v =
-  PresentEntry
+  Entry
     Int {-^ Amount of instances in deque. -}
     k v
-  |
-  MissingEntry Int k
 
 lookup :: (Hashable k, Eq k) => k -> LookupOrderedHashMap k v -> (Maybe v, LookupOrderedHashMap k v)
 lookup key (LookupOrderedHashMap deque trie) =
-  Hamt.revision (hash key) (selectEntry key) miss update trie
-    & second (fromMaybe Hamt.empty)
-    & second (LookupOrderedHashMap (Deque.snoc key deque))
+  revisionHamt key miss update trie
+    & \(cont, trieMaybe) -> cont (fromMaybe Hamt.empty trieMaybe)
   where
     miss =
-      (Nothing, Just (MissingEntry 1 key))
-    update =
-      \case
-        PresentEntry count entryKey value ->
-          (Just value, Just (PresentEntry (succ count) entryKey value))
-        MissingEntry count entryKey ->
-          (Nothing, Just (MissingEntry (succ count) entryKey))
+      (
+        \trie -> (Nothing, LookupOrderedHashMap deque trie)
+        ,
+        Nothing
+        )
+    update (Entry count entryKey value) =
+      (
+        \trie -> (Just value, LookupOrderedHashMap (Deque.snoc entryKey deque) trie)
+        ,
+        Just (Entry (succ count) entryKey value)
+        )
 
 {-|
 Evict one entry from the map.
@@ -48,36 +49,38 @@ evict (LookupOrderedHashMap deque trie) =
     iterate deque trie =
       case Deque.uncons deque of
         Just (key, nextDeque) ->
-          Hamt.revision (hash key) (selectEntry key) miss update trie
-            & \(cont, hamt) -> cont (fromMaybe Hamt.empty hamt)
+          revisionHamt key miss update trie
+            & \(cont, trieMaybe) -> cont (fromMaybe Hamt.empty trieMaybe)
           where
             miss =
               (iterate nextDeque, Nothing)
             update =
               \case
-                PresentEntry count entryKey value ->
+                Entry count entryKey value ->
                   if count == 1
                     then
-                      (\trie -> (Just (entryKey, value), LookupOrderedHashMap nextDeque trie), Nothing)
+                      (\trie -> (Just (entryKey, value), LookupOrderedHashMap nextDeque trie),
+                        Nothing)
                     else
-                      (iterate nextDeque, Just (PresentEntry (pred count) entryKey value))
-                MissingEntry count entryKey ->
-                  if count == 1
-                    then
-                      (iterate nextDeque, Nothing)
-                    else
-                      (iterate nextDeque, Just (MissingEntry (pred count) entryKey))
+                      (iterate nextDeque,
+                        Just (Entry (pred count) entryKey value))
         Nothing ->
           (Nothing, LookupOrderedHashMap deque trie)
 
 entryKey :: Entry k v -> k
 entryKey =
   \case
-    PresentEntry count entryKey value -> entryKey
-    MissingEntry count entryKey -> entryKey
+    Entry count entryKey value -> entryKey
 
 selectEntry :: Eq k => k -> Entry k v -> Maybe (Entry k v)
 selectEntry key entry =
   if entryKey entry == key
     then Just entry
     else Nothing
+
+revisionHamt :: (Functor f, Hashable k, Eq k) =>
+  k ->
+  f (Maybe (Entry k v)) -> (Entry k v -> f (Maybe (Entry k v))) ->
+  Hamt.Hamt (Entry k v) -> f (Maybe (Hamt.Hamt (Entry k v)))
+revisionHamt key =
+  Hamt.revision (hash key) (selectEntry key)
