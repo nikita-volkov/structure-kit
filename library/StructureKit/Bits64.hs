5aa581d7fbd7f7c46cc5b83fde311aea10f8cd56
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-02-22 18:03:51 +0300

    Add Bits64 revision

--- library/StructureKit/Bits64.hs
+++ library/StructureKit/Bits64.hs
@@ -8,6 +8,7 @@ module StructureKit.Bits64
   singleton,
   lookup,
   insert,
+  revision,
 )
 where
 
@@ -45,3 +46,32 @@ insert value (Bits64 word) =
     newWord = word .|. bitAtValue
     newBitSet = if newWord == word then Nothing else Just (Bits64 newWord)
     in (fromIntegral bitAtValue, newBitSet)
+
+revision :: Functor f => Int -> f Bool -> (Int -> f Bool) -> Bits64 -> f (Maybe Bits64)
+revision value onMissing onPresent (Bits64 word) =
+  if value == 0
+    then choose 1 0
+    else let
+      bitAtValue = bit value
+      in choose bitAtValue (popCount (word .&. (bitAtValue - 1))) 
+  where
+    choose bitAtValue populatedIndex =
+      if word .&. bitAtValue /= 0
+        then
+          onPresent populatedIndex
+            & fmap (\case
+                True -> Just (Bits64 word)
+                False -> let
+                  newWord = xor word bitAtValue
+                  in if newWord == 0
+                    then Nothing
+                    else Just (Bits64 newWord))
+        else
+          onMissing
+            & fmap (\case
+                True -> let
+                  newWord = word .|. bitAtValue
+                  in Just (Bits64 newWord)
+                False -> if word == 0
+                  then Nothing
+                  else Just (Bits64 word))
