4930f54412ecc7b727fafc2dc88ebaa4e05bace4
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-09-02 10:56:52 +0300

    Add remaining docs

--- library/StructureKit/IdRegistry.hs
+++ library/StructureKit/IdRegistry.hs
@@ -11,15 +11,21 @@ import qualified Data.IntMap.Strict as IntMap
 import qualified StructureKit.IdGen as IdGen
 import StructureKit.Prelude hiding (empty, lookup)
 
+-- |
+-- Registry of values that automatically generates keys.
 data IdRegistry a
   = IdRegistry
       IdGen.IdGen
       (IntMap a)
   deriving (Functor, Foldable, Traversable)
 
+-- |
+-- Create an empty registry.
 empty :: IdRegistry a
 empty = IdRegistry IdGen.empty IntMap.empty
 
+-- |
+-- Register a value and produce a key that can be later used to retrieve this value.
 register :: a -> IdRegistry a -> (Int, IdRegistry a)
 register payload (IdRegistry gen map) =
   let (id, newGen) = IdGen.fetch gen
