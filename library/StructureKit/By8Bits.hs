c38727532e35b0cb9f524d7218f3b07026674474
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-09-04 11:52:20 +0300

    Implement selection API in By8Bits

--- library/StructureKit/By8Bits.hs
+++ library/StructureKit/By8Bits.hs
@@ -23,7 +23,7 @@ empty =
 lookup :: Int -> By8Bits a -> Maybe a
 lookup key (By8Bits a b c d) =
   By6Bits.lookup
-    key
+    (error "TODO")
     if key < 128
       then
         if key < 64
@@ -40,11 +40,11 @@ insert key value (By8Bits a b c d) =
     then
       if key < 64
         then By6Bits.insert key value a & second (\a -> By8Bits a b c d)
-        else By6Bits.insert key value b & second (\b -> By8Bits a b c d)
+        else By6Bits.insert (error "TODO") value b & second (\b -> By8Bits a b c d)
     else
       if key < 192
-        then By6Bits.insert key value c & second (\c -> By8Bits a b c d)
-        else By6Bits.insert key value d & second (\d -> By8Bits a b c d)
+        then By6Bits.insert (error "TODO") value c & second (\c -> By8Bits a b c d)
+        else By6Bits.insert (error "TODO") value d & second (\d -> By8Bits a b c d)
 
 adjust :: (a -> a) -> Int -> By8Bits a -> By8Bits a
 adjust =
@@ -71,3 +71,59 @@ foldrWithKey step end (By8Bits part1 part2 part3 part4) =
   where
     offsetStep offset key =
       step (key + offset)
+
+-- * Selection API
+
+select :: Int -> By8Bits a -> Either (Missing a) (Present a)
+select key (By8Bits a b c d) =
+  if key < 128
+    then
+      if key < 64
+        then case By6Bits.select key a of
+          Right present -> Right $ APresent present b c d
+          Left missing -> Left $ AMissing missing b c d
+        else case By6Bits.select (key - 64) b of
+          Right present -> Right $ BPresent a present c d
+          Left missing -> Left $ BMissing a missing c d
+    else
+      if key < 192
+        then case By6Bits.select (key - 128) c of
+          Right present -> Right $ CPresent a b present d
+          Left missing -> Left $ CMissing a b missing d
+        else case By6Bits.select (key - 192) d of
+          Right present -> Right $ DPresent a b c present
+          Left missing -> Left $ DMissing a b c missing
+
+-- **
+
+data Present a
+  = APresent (By6Bits.Present a) (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.By6Bits a)
+  | BPresent (By6Bits.By6Bits a) (By6Bits.Present a) (By6Bits.By6Bits a) (By6Bits.By6Bits a)
+  | CPresent (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.Present a) (By6Bits.By6Bits a)
+  | DPresent (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.Present a)
+
+read :: Present a -> a
+read = \case
+  APresent present b c d ->
+    By6Bits.read present
+
+remove :: Present a -> By8Bits a
+remove = \case
+  APresent present b c d ->
+    By8Bits (By6Bits.remove present) b c d
+
+overwrite :: a -> Present a -> By8Bits a
+overwrite =
+  error "TODO"
+
+-- **
+
+data Missing a
+  = AMissing (By6Bits.Missing a) (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.By6Bits a)
+  | BMissing (By6Bits.By6Bits a) (By6Bits.Missing a) (By6Bits.By6Bits a) (By6Bits.By6Bits a)
+  | CMissing (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.Missing a) (By6Bits.By6Bits a)
+  | DMissing (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.Missing a)
+
+write :: a -> Missing a -> By8Bits a
+write =
+  error "TODO"
