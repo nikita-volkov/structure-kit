40a81cbdd6f2bbff9d2f9e7a7229112f7741ce9c
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-09-07 21:40:49 +0300

    Generalise the build script

--- build.bash
+++ build.bash
@@ -45,4 +45,4 @@ ormolu --mode inplace -c \
 $(find . -name '*.hs')
 
 stack build --fast --test \
---ta "--quickcheck-replay 0 --quickcheck-tests 99 -j1 -p \"/Bits64/&&/Removed entry is inaccessible/\""
+--ta "--quickcheck-replay 0 --quickcheck-tests 99 -j1"
