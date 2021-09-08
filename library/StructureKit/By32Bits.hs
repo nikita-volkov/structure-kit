49e53487e6f8e19d0ec3e665907f2b75e3e64494
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-09-08 11:28:01 +0300

    Add a cost center on By32Bits

--- library/StructureKit/By32Bits.hs
+++ library/StructureKit/By32Bits.hs
@@ -85,6 +85,7 @@ null (By32Bits tree1) = By6Bits.null tree1
 
 locate :: Int -> By32Bits a -> Either (Missing a) (Existing a)
 locate key (By32Bits tree1) =
+  {-# SCC "locate" #-}
   case By6Bits.locate key1 tree1 of
     Right present1 ->
       let remove1 = By32Bits $ By6Bits.remove present1
