1a990067e1284406362bb733e16248ddc869de09
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-09-08 11:30:18 +0300

    Reduce allocation by making the fields of By8Bits strict

--- library/StructureKit/By8Bits.hs
+++ library/StructureKit/By8Bits.hs
@@ -30,7 +30,11 @@ import StructureKit.Prelude hiding (empty, insert, lookup, null, read, remove, s
 -- |
 -- Map indexed with 8 bits.
 data By8Bits a
-  = By8Bits (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.By6Bits a) (By6Bits.By6Bits a)
+  = By8Bits
+      !(By6Bits.By6Bits a)
+      !(By6Bits.By6Bits a)
+      !(By6Bits.By6Bits a)
+      !(By6Bits.By6Bits a)
   deriving (Functor)
 
 instance NFData a => NFData (By8Bits a) where
