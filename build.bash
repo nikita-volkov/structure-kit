9b2cc89b9dd62b2d4b75d08167798c117020de39
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-09-08 11:20:49 +0300

    Update the build script

--- build.bash
+++ build.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
+function drop_first_line_in_file {
+  tail -n +2 "$1" > "$1.tmp" && mv "$1.tmp" "$1"
+}
+
 ormolu --mode inplace -c \
 -o -XBangPatterns \
 -o -XConstraintKinds \
@@ -44,9 +48,14 @@ ormolu --mode inplace -c \
 -o -XUnboxedTuples \
 $(find . -name '*.hs')
 
-stack \
-build \
---fast \
---bench \
---no-run-benchmarks \
---test
+time_allocation_report_file="profile/time-allocation-report"
+gc_report_file="profile/gc-report"
+
+stack build \
+--ghc-options "-O2 -rtsopts -fprof-auto" \
+--profile \
+structure-kit:profile --ta "+RTS -p -po$time_allocation_report_file -s$gc_report_file -V0.0001 -RTS"
+
+drop_first_line_in_file $gc_report_file
+
+profiteur $time_allocation_report_file.prof
