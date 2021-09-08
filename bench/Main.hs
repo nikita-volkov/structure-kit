f99897054d471f640712517c38abde3a60cbe54d
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-09-08 10:25:58 +0300

    Add comparison benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,3 +1,5 @@
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Map.Strict as Map
 import Gauge.Main
 import qualified Main.ExtrasFor.LruHashCache as LruHashCacheExtras
 import qualified Main.ExtrasFor.QcGen as GenExtras
@@ -7,12 +9,7 @@ import qualified Test.QuickCheck.Random as Random
 import Prelude
 
 main =
-  defaultMain
-    [ bgroup "lru-hash-cache" lruHashCache
-    ]
-
-lruHashCache =
-  GenExtras.seedGen gen 0
+  defaultMain $ GenExtras.seedGen gen 0
   where
     gen = do
       inserts <- replicateM 1000 $ do
@@ -21,11 +18,28 @@ lruHashCache =
         return (key, val)
       let !lhc = snd $ LruHashCacheExtras.insertMany inserts $ LruHashCache.empty 500
           !existingKey = fst $ inserts !! 777
+          !hashMap = HashMap.fromList inserts
+          !map = Map.fromList inserts
       case LruHashCache.lookup existingKey lhc of
         (Nothing, _) -> error "Key not found"
         _ -> return ()
       return $
-        [ bench "lookup" $ nf (LruHashCache.lookup existingKey) lhc,
-          bench "insert-missing" $ nf (LruHashCache.insert "something new" 7) lhc,
-          bench "insert-existing" $ nf (LruHashCache.insert existingKey 7) lhc
+        [ bgroup
+            "lru-hash-cache"
+            [ bench "lookup" $ nf (LruHashCache.lookup existingKey) lhc,
+              bench "insert-missing" $ nf (LruHashCache.insert "something new" 7) lhc,
+              bench "insert-existing" $ nf (LruHashCache.insert existingKey 7) lhc
+            ],
+          bgroup
+            "std-hash-map"
+            [ bench "lookup" $ nf (HashMap.lookup existingKey) hashMap,
+              bench "insert-missing" $ nf (HashMap.insert "something new" 7) hashMap,
+              bench "insert-existing" $ nf (HashMap.insert existingKey 7) hashMap
+            ],
+          bgroup
+            "std-map"
+            [ bench "lookup" $ nf (Map.lookup existingKey) map,
+              bench "insert-missing" $ nf (Map.insert "something new" 7) map,
+              bench "insert-existing" $ nf (Map.insert existingKey 7) map
+            ]
         ]
