46565129c06f4894e12e5d1759b0326f4eef67b0
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-09-09 15:19:50 +0300

    Isolate the formatting script and focus the build scripts

--- test.bash
+++ test.bash
@@ -1,49 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-ormolu --mode inplace -c \
--o -XBangPatterns \
--o -XConstraintKinds \
--o -XDataKinds \
--o -XDefaultSignatures \
--o -XDeriveDataTypeable \
--o -XDeriveFoldable \
--o -XDeriveFunctor \
--o -XDeriveGeneric \
--o -XDeriveTraversable \
--o -XDerivingVia \
--o -XDuplicateRecordFields \
--o -XEmptyDataDecls \
--o -XFlexibleContexts \
--o -XFlexibleInstances \
--o -XFunctionalDependencies \
--o -XGADTs \
--o -XGeneralizedNewtypeDeriving \
--o -XLambdaCase \
--o -XLiberalTypeSynonyms \
--o -XMagicHash \
--o -XMultiParamTypeClasses \
--o -XMultiWayIf \
--o -XNoImplicitPrelude \
--o -XNoMonomorphismRestriction \
--o -XOverloadedLabels \
--o -XOverloadedStrings \
--o -XPatternGuards \
--o -XPatternSynonyms \
--o -XParallelListComp \
--o -XQuasiQuotes \
--o -XRankNTypes \
--o -XRecordWildCards \
--o -XScopedTypeVariables \
--o -XStandaloneDeriving \
--o -XTemplateHaskell \
--o -XTupleSections \
--o -XTypeApplications \
--o -XTypeFamilies \
--o -XTypeOperators \
--o -XUnboxedTuples \
-$(find . -name '*.hs')
-
 stack \
 --work-dir .test.stack-work \
 build \
