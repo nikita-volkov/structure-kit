1aa0f4cb1702dddad61fe9bb518e8945c93d86b3
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-04 00:34:50 +0300

    Add a (from/to)String test

--- test/StructureKit/TestSuites/Charset.hs
+++ test/StructureKit/TestSuites/Charset.hs
@@ -1,5 +1,7 @@
 module StructureKit.TestSuites.Charset where
 
+import Data.String.ToString as Exports (ToString (..))
+import StructureKit.Charset (Charset)
 import qualified StructureKit.Charset as Charset
 import qualified Test.QuickCheck as QuickCheck
 import Test.QuickCheck.Instances
@@ -10,4 +12,7 @@ import Test.Tasty.Runners
 import Prelude hiding (assert)
 
 tests =
-  []
+  [ testProperty "(from/to)String" $ \string ->
+      let reference = nub (sort string)
+       in reference === toString (fromString @Charset string)
+  ]
