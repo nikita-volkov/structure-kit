91b7073d78ed413aa6666fa13c4a5ac941bc75e2
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-04-07 20:25:34 +0300

    Add Range

---
+++ library/StructureKit/Range.hs
@@ -0,0 +1,46 @@
+module StructureKit.Range
+  ( -- *
+    Range (..),
+
+    -- **
+    toPredicate,
+
+    -- **
+    isEmpty,
+  )
+where
+
+import StructureKit.Prelude
+
+-- *
+
+data Range a
+  = Range
+      !a
+      -- ^ Equal or larger than.
+      !a
+      -- ^ Smaller than.
+
+instance Ord a => Semigroup (Range a) where
+  Range lMin lMax <> Range rMin rMax =
+    Range (max lMin rMin) (min lMax rMax)
+
+instance (Bounded a, Ord a) => Monoid (Range a) where
+  mempty =
+    Range minBound maxBound
+
+-- *
+
+-- |
+-- Check whether the range contains the element.
+toPredicate :: Ord a => Range a -> a -> Bool
+toPredicate (Range min max) a =
+  a >= min && a < max
+
+-- *
+
+-- |
+-- Checks whether the range includes any elements at all.
+isEmpty :: Ord a => Range a -> Bool
+isEmpty (Range min max) =
+  min >= max
